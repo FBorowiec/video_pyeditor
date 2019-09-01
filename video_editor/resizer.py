d0f666f00f42c10e2f3f02f1c2a2a88ffc52ae04
F Borowiec <franek.borowiec@gmail.com>
2019-09-01 10:25:06 +0200

    Relocated input parameters from main into each function

--- video_editor/resizer.py
+++ video_editor/resizer.py
@@ -1,5 +1,7 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_resize
 
-def resize_video(video, size, targetname="videos/video_resized.mp4"):
-  ffmpeg_resize(video, output=targetname, size=size)
+def resize_video(video, targetname="videos/video_resized.mp4"):
+  size = int(input("Please choose size: [720, 1080]: "))
+  size_tuple = (size, size/(16/9))
+  ffmpeg_resize(video, output=targetname, size=size_tuple)
   return "Video resized successfully!"
--- video_editor/trimmer.py
+++ video_editor/trimmer.py
@@ -1,5 +1,7 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-def trim_video(video, start_t, end_t, targetname="videos/video_trimmed.mp4"):
+def trim_video(video, targetname="videos/video_trimmed.mp4"):
+  start_t = int(input("start time (sec): "))
+  end_t = int(input("end time (sec): "))
   ffmpeg_extract_subclip(video, start_t, end_t, targetname=targetname)
   return "Video trimmed successfully!"
