bd1af52f7c7fa320c61d6f4a3680156ab207db93
F Borowiec <franek.borowiec@gmail.com>
2019-09-02 23:21:15 +0200

    Updated the setup

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+import imageio
+import subprocess
 
-!{sys.executable} -m pip install -r requirements.txt
-!{sys.executable} -m pip install --trusted-host pypi.python.org moviepy
-imageio.plugins.ffmpeg.download()
+#pip install -r requirements.txt
+#pip install --trusted-host pypi.python.org moviepy
+#imageio.plugins.ffmpeg.download()
+
+subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+subprocess.call(['pip', 'install', '--trusted-host', 'pypi.python.org', 'moviepy'])
+subprocess.call(['pip', 'install', 'imageio-ffmpeg'])
