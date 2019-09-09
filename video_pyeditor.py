91ece19dd4ab9fe107c7583a13c50e837f8c76ca
F.Borowiec <franek.borowiec@gmail.com>
2019-09-09 18:56:49 +0200

    Added an interface so the user can choose how many operations to perform on a single video

--- video_pyeditor.py
+++ video_pyeditor.py
@@ -9,9 +9,40 @@ from video_editor import metadata_editor
 from video_editor import joiner
 from video_editor import cropper
 
+def interface(input_choice, video, video_name):
+  if (input_choice == 1):
+    rotator.rotate_video(video, video_name=video_name)
+    video_name = video_name + '_rotated'
+  elif (input_choice == 2):
+    trimmer.trim_video(video, video_name=video_name)
+    video_name = video_name + '_trimmed'
+  elif (input_choice == 3):
+    formatter.format(video, video_name=video_name)
+    video_name = video_name + '_formatted'
+  elif (input_choice == 4):
+    resizer.resize_video(video, video_name=video_name)
+    video_name = video_name + '_resized'
+  elif (input_choice == 5):
+    metadata_editor.date_changer(video, video_name=video_name)
+    video_name = video_name + '_dated'
+  elif (input_choice == 6):
+    joiner.join_videos(video, video_name=video_name)
+    video_name = video_name + '_joined'
+  elif (input_choice == 7):
+    cropper.crop(video, video_name=video_name)
+    video_name = video_name + '_cropped'
+  else:
+    print("Invalid choice!")
+  return video_name
+
 def main():
   print(" VIDEO EDITOR")
-  i1 = int(input("  1. Rotate a video\n\
+  
+  video_name = input("Please enter your mp4 video name (without the extension): ")
+  
+  proceed = True
+  while (proceed):
+    input_choice = int(input("  1. Rotate a video\n\
   2. Trim a video\n\
   3. Change video aspect\n\
   4. Resize a video\n\
@@ -19,27 +50,10 @@ def main():
   6. Concatenate two videos \n\
   7. Crop a video \n\
   [1/2/3/4/5/6/7]: "))
-  
-  video_name = input("Please enter your mp4 video name (without the extension): ")
-  video = os.getcwd() + '/videos/' + video_name + '.mp4'
-  print(video + " is being processed...")
-  
-  if (i1 == 1):
-    rotator.rotate_video(video, video_name=video_name)
-  elif (i1 == 2):
-    trimmer.trim_video(video, video_name=video_name)
-  elif (i1 == 3):
-    formatter.format(video, video_name=video_name)
-  elif (i1 == 4):
-    resizer.resize_video(video, video_name=video_name)
-  elif (i1 == 5):
-    metadata_editor.date_changer(video, video_name=video_name)
-  elif (i1 == 6):
-    joiner.join_videos(video, video_name=video_name)
-  elif (i1 == 7):
-    cropper.crop(video, video_name=video_name)
-  else:
-    print("Invalid choice!")
+    video = os.getcwd() + '/videos/' + video_name + '.mp4'
+    print(video + " is being processed...")
+    video_name = interface(input_choice, video=video, video_name=video_name)
+    proceed = bool(input("Do you want to proceed? [True, False]: "))
 
 if (__name__ == '__main__'):
   main()
