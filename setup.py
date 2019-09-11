15e6e3f5192a316093873400a6b271134e854b04
F.Borowiec <franek.borowiec@gmail.com>
2019-09-11 10:51:54 +0200

    Updated requirements and setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@
 import sys
 import subprocess
 
-#pip install -r requirements.txt
-#pip install --trusted-host pypi.python.org moviepy
-#imageio.plugins.ffmpeg.download()
-
-subprocess.call(['pip3', 'install', 'imageio'])
 subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
-subprocess.call(['pip3', 'install', '--trusted-host', 'pypi.python.org', 'moviepy'])
-subprocess.call(['pip3', 'install', 'imageio-ffmpeg'])
-subprocess.call(['pip3', 'install', '--upgrade', 'mss'])
-subprocess.call(['pip3', 'install', 'pyscreenshot'])
+#subprocess.call(['pip3', 'install', '--trusted-host', 'pypi.python.org', 'moviepy'])
+#subprocess.call(['pip3', 'install', '--upgrade', 'mss'])
--- terminal_video_editor.py
+++ terminal_video_editor.py
@@ -34,7 +34,6 @@ def interface(input_choice, video, video_name):
     cropper.crop(video, video_name=video_name)
     video_name = video_name + '_cropped'
   elif (input_choice == 8):
-    print("Please press 'q' to exit!")
     recorder.screen_record(video_name=video_name)
   else:
     print("Invalid choice!")
