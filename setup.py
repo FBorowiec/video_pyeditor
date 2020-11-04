cb8d2d96565c38b96f2fdd906fc140ed181cf8b5
Franek Borowiec <franek.borowiec@gmail.com>
2020-11-04 21:50:29 +0100

    Remove setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-import subprocess
-
-subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
-#subprocess.call(['pip3', 'install', '--trusted-host', 'pypi.python.org', 'moviepy'])
-#subprocess.call(['pip3', 'install', '--upgrade', 'mss'])
